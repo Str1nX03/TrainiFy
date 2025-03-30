3e8d608aff68b369b1d3a76a312965e13315e557
Str1nX03 <draveen2210@gmail.com>
2025-03-30 12:35:47 +0530

    Setup done

---
+++ app.py
@@ -0,0 +1,11 @@
+from src.exception import CustomException
+from src.logger import logging
+from src.ml_algorithms.k_means import KMeans
+from src.ml_algorithms.knn import KNN
+from src.ml_algorithms.linear_regression import Linear_Regression
+from src.ml_algorithms.logistic_regression import Logistic_Regression
+from src.ml_algorithms.nbayes import NaiveBayes
+from src.ml_algorithms.decision_tree import DecisionTree, Node
+from src.ml_algorithms.random_forest import RandomForest
+from src.ml_algorithms.pca import PCA
+from src.ml_algorithms.svm import SVM_classifier
